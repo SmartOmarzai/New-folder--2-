855a977ef2d3e64fd9d8d5d3be644088ac3259d2
mohabat <mohabatullahomarzai@gimal.com>
2023-12-16 22:10:31 -0800

    added the file

---
+++ object.js
@@ -0,0 +1,17 @@
+const circle = {
+    radius: 2,
+    location: {
+        x : 5,
+        y : 12
+
+    },
+    draw: function() {
+        console.log('draw');
+    }
+
+};
+
+circle.draw();
+
+
+
